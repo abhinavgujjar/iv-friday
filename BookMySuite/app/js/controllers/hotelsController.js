d790d4aafe61fb67ccd37081a9f66c3bad05525d
abhinav <abhinavgujjar@gmail.com>
2014-02-21 18:30:29 +0530

    custom filter

--- BookMySuite/app/js/controllers/hotelsController.js
+++ BookMySuite/app/js/controllers/hotelsController.js
@@ -3,34 +3,47 @@ bookMySuite.controller('hotelsController', function($scope){
 
 	$scope.allowRegister = true;
 
+	$scope.upVote = function(hotel){
+		hotel.rating++;
+	}
+
+	$scope.downVote = function(hotel){
+		hotel.rating--;
+	}
+
 	$scope.hotels = [{
 		name : 'Radisson',
 		location: 'Noida',
 		rating: 10,
+		dailyRate: 300,
 		image: 'img/radisson.jpg'
 	},
 	{
 		name: 'Park Plaza',
 		location: 'Sector 52',
 		image: 'img/taj.jpg',
+		dailyRate: 450,
 		rating: 6
 	},
 	{
 		name: 'Hilton',
 		location: 'Janakpuri',
 		image: 'img/ashok.jpg',
+		dailyRate: 700,
 		rating: 4
 	},
 	{
 		name: "Country Inn",
 		location: "Dubai",
 		image: "img/countryinn.jpg",
+		dailyRate: 1000,
 		rating: 3
 	},
 	{
 		name: "Royal Orchid",
 		location: "Bangalore",
 		rating: 2,
+		dailyRate: 200,
 		image: "img/royalorchid.jpg",
 	},
 	{
@@ -38,6 +51,7 @@ bookMySuite.controller('hotelsController', function($scope){
 		name : "Lalit  Ashok",
 		location : "Delhi",
 		rating: 5,
+		dailyRate: 4000,
 		image: "img/ashok.jpg",
 	}
 	];
--- BookMySuite/app/js/filters.js
+++ BookMySuite/app/js/filters.js
@@ -2,9 +2,8 @@
 
 /* Filters */
 
-angular.module('myApp.filters', []).
-  filter('interpolate', ['version', function(version) {
-    return function(text) {
-      return String(text).replace(/\%VERSION\%/mg, version);
-    }
-  }]);
+bookMySuite.filter('toRupees', function(){
+	return function(dollarRate){
+		return dollarRate * 60;
+	}
+})
