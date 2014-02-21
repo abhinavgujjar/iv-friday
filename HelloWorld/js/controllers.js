b0c95ef95fd7fbd53b74bd88b6de0378a5c30747
abhinav <abhinavgujjar@gmail.com>
2014-02-21 15:40:03 +0530

    images

--- HelloWorld/js/controllers.js
+++ HelloWorld/js/controllers.js
@@ -1,6 +1,22 @@
 helloApp.controller('worldController', function($scope){
 	$scope.message = "Yay!! Weekend";
 
+	$scope.hotels = [{
+		name : 'Radisson',
+		location: 'Noida',
+		image: 'img/radisson.jpg'
+	},
+	{
+		name: 'Park Plaza',
+		location: 'Sector 52',
+		image: 'img/taj.jpg'
+	},
+	{
+		name: 'Hilton',
+		location: 'Janakpuri',
+		image: 'img/ashok.jpg'
+	}];
+
 	$scope.yell = function(input)
 	{
 		if (input)
