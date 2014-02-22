69d96cd8ba949b68742f1d6563f6a95fc15415d7
abhinav <abhinavgujjar@gmail.com>
2014-02-22 12:52:03 +0530

    validations

--- BookMySuite/app/js/controllers/newHotelController.js
+++ BookMySuite/app/js/controllers/newHotelController.js
@@ -1,5 +1,16 @@
 bookMySuite.controller('newHotelController', function($scope){
-	$scope.save = function(hotel){
+	$scope.save = function(hotel, newHotelForm){
+
+		console.log(newHotelForm);
+		if ( newHotelForm.$valid )
+		{
+			alert('YES');
+		}
+		else
+		{
+			alert('NO');
+		}
+
 		console.log('new hotel' + hotel);
 		console.log('name ' + hotel.name);
 		console.log('location' + hotel.location);
